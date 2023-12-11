189c8f6c24ca572aa622aa8a7c8865f49c8d1606
Anne <149403890+AnneRguez@users.noreply.github.com>
2023-12-11 15:24:26 +0100

    Update streamlitpruebas.py

--- streamlitpruebas.py
+++ streamlitpruebas.py
@@ -1,9 +1,35 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-
+data= pd.DataFrame(np.random.randn(100,2),columns=['A','B'])
 
 st.header("App Anne")
+st.write(data.head(10))
+st.header("Grafico 1")
+fig, ax=plt.subplots()
+ax.scatter(data['A'],data['B'])
+st.pyplot(fig)
+bar=st.progress(0)
+time.sleep(2)
+bar.progress(100)
 
-boton=st.button("click aqui")
-if boton:
-    st.balloons()
+
+#barra lateral
+st.sidebar.header("Progress")
+bar=st.sidebar.slider(
+    label=("Barra"),
+    min_value=0,
+    max_value=100)
+
+
+if bar < 50:
+    st.sidebar.error("Suspenso")
+    #st.sidebar.warning()
+else:
+    st.sidebar.success("Aprobado")  
+    #st.sidebar.success()  
+
+st.selectbox("Selecciona una letra", ["A","B","C"])
+
